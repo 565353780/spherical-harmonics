108325595aee8aa3213777db531bfe5e21320659
chLi <565353780@qq.com>
2023-11-23 15:01:31 +0800

    simplify getDirections

--- spherical_harmonics/Method/direction.py
+++ spherical_harmonics/Method/direction.py
@@ -10,10 +10,6 @@ def getDirections(phi_array, theta_array):
     directions = np.zeros([phi_array.shape[0], theta_array.shape[0], 3])
     for i in range(phi_array.shape[0]):
         for j in range(theta_array.shape[0]):
-            directions[i][j] = [
-                np.sin(theta_array[j]) * np.sin(phi_array[i]),
-                np.sin(theta_array[j]) * np.cos(phi_array[i]),
-                np.cos(theta_array[j]),
-            ]
+            directions[i][j] = getDirection(phi_array[i], theta_array[j])
 
     return directions
