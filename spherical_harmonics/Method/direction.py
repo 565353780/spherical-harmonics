d02b9f720720f9af8bf58ddeb5e9361846642538
chLi <565353780@qq.com>
2023-11-24 18:14:05 +0800

    add get param func

--- spherical_harmonics/Method/direction.py
+++ spherical_harmonics/Method/direction.py
@@ -13,3 +13,25 @@ def getDirections(phi_array, theta_array):
             directions[i][j] = getDirection(phi_array[i], theta_array[j])
 
     return directions
+
+def getParam(direction):
+    norm = np.linalg.norm(direction)
+
+    if np.linalg.norm(direction) == 0:
+        print('[ERROR][direction::getParam]')
+        print('\t direction is 0 vector!')
+        return None, None
+
+    norm_direction = np.array(direction) / norm
+
+    theta = np.arccos(norm_direction[2])
+
+    sin_theta = np.sin(theta)
+    if sin_theta == 0:
+        return 0, theta
+
+    phi = np.arctan2(norm_direction[0] / sin_theta, norm_direction[1] / sin_theta)
+    if phi < 0:
+        phi += 2.0 * np.pi
+
+    return phi, theta
