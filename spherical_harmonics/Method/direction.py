dcbf2b31d448a27738e2e284f8a471e1cd8b5cda
chLi <565353780@qq.com>
2023-12-20 19:41:02 +0800

    add getParams func

--- spherical_harmonics/Method/direction.py
+++ spherical_harmonics/Method/direction.py
@@ -17,7 +17,7 @@ def getDirections(phi_array, theta_array):
 def getParam(direction):
     norm = np.linalg.norm(direction)
 
-    if np.linalg.norm(direction) == 0:
+    if norm == 0:
         print('[ERROR][direction::getParam]')
         print('\t direction is 0 vector!')
         return None, None
@@ -35,3 +35,33 @@ def getParam(direction):
         phi += 2.0 * np.pi
 
     return phi, theta
+
+def getParams(directions):
+    norm = np.linalg.norm(directions, axis=1)
+
+    valid_mask = norm > 0
+
+    valid_norm = norm[valid_mask]
+
+    valid_norm = np.vstack([valid_norm, valid_norm, valid_norm]).transpose(1, 0)
+
+    norm_directions = np.zeros_like(directions, dtype=float)
+    norm_directions[:, 2] = 1.0
+
+    norm_directions[valid_mask] = directions[valid_mask] / valid_norm
+
+    thetas = np.arccos(norm_directions[:, 2])
+
+    sin_thetas = np.sin(thetas[valid_mask])
+
+    phis = np.zeros_like(thetas, dtype=float)
+
+    phis[valid_mask] = np.arctan2(
+        norm_directions[valid_mask][:, 0] / sin_thetas,
+        norm_directions[valid_mask][:, 1] / sin_thetas)
+
+    add_idx = phis < 0
+
+    phis[add_idx] += 2.0 * np.pi
+
+    return phis, thetas
