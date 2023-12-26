f5373d5db46737dfb7247c937240c2e1432f9a52
chLi <565353780@qq.com>
2023-12-26 23:54:51 +0800

    update demo func

--- spherical_harmonics/Demo/render.py
+++ spherical_harmonics/Demo/render.py
@@ -1,10 +1,11 @@
-from spherical_harmonics.Data.sh_model import SHModel
+from spherical_harmonics.Data.sh_3d_model import SH3DModel
 
 def demo():
     degree_max = 3
     params = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-    method_name = 'jittor'
+    method_name = 'numpy'
 
-    sh_model = SHModel(degree_max, params, method_name)
+    sh_model = SH3DModel(degree_max, method_name)
+    sh_model.setParams(params)
     sh_model.render()
     return True
