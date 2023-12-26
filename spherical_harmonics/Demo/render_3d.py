3624517534cde1dbaea39b7fd88a62ec833089ee
chLi <565353780@qq.com>
2023-12-27 02:27:14 +0800

    activate all render case

--- spherical_harmonics/Demo/render_3d.py
+++ spherical_harmonics/Demo/render_3d.py
@@ -2,7 +2,7 @@ from spherical_harmonics.Data.sh_3d_model import SH3DModel
 
 def demo():
     degree_max = 3
-    params = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    params = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
     method_name = 'numpy'
 
     sh_3d_model = SH3DModel(degree_max, method_name)
